33f839583018ecd45442bb3511ef2a5f51c08fe2
LBwaks <obwakuvictor@gmail.com>
2023-09-16 07:34:10 +0300

    bid model and and admin

--- Bid/admin.py
+++ Bid/admin.py
@@ -1,3 +1,22 @@
+from typing import Any
 from django.contrib import admin
+from .models import Bid 
 
 # Register your models here.
+@admin.register(Bid)
+class BidAdmin(admin.ModelAdmin):
+    '''Admin View for Bid'''
+
+    list_display = ('user','produce','charge','status','created')
+    # list_filter = ('',)
+    # inlines = [
+    #     Inline,
+    # ]
+    # raw_id_fields = ('user',)
+    readonly_fields = ('user',)
+    # search_fields = ('',)
+    # date_hierarchy = ''
+    # ordering = ('',)
+    def save_model(self, request: Any, obj: Any, form: Any, change: Any) -> None:
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
--- Bid/models.py
+++ Bid/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User 
 from django_extensions.db.fields import AutoSlugField
+import uuid
 from ckeditor.fields import RichTextField
 from django.utils.translation import gettext as _
+from Produce.models import Produce
 # Create your models here.
 class Bid(models.Model):
     """Model definition for Bid."""
 
     # TODO: Define fields here
+    uuid =models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
+    user =models.ForeignKey(User, verbose_name=_(""), on_delete=models.CASCADE)
+    produce = models.ForeignKey(Produce, verbose_name=_(""), on_delete=models.CASCADE)
+    charge =models.IntegerField(_("Charge"))
+    reasons =RichTextField(_("Reasons For you charge"))
+    services =RichTextField(_("Other secondary Services "))
+    status = models.CharField(_(""),default="PENDING", max_length=50)
+    approved_cancelled_time =models.DateTimeField(_(""),blank=True, null=True)
+    created = models.DateTimeField( auto_now=False, auto_now_add=True)
 
     class Meta:
         """Meta definition for Bid."""
@@ -17,11 +28,11 @@ class Bid(models.Model):
 
     def __str__(self):
         """Unicode representation of Bid."""
-        pass
+        return f"Produce {self.produce.title}, has a bid of {self.charge}"
 
-    def save(self):
-        """Save method for Bid."""
-        pass
+    # def save(self):
+    #     """Save method for Bid."""
+    #     pass
 
     def get_absolute_url(self):
         """Return absolute url for Bid."""
